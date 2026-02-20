4f4b3cf24ecb69912c3c9a23ea571de0ef947cea
helena-cruse <helena.m.cruse@gmail.com>
2026-02-20 07:54:58 +0100

    chore: init Next.js app and project structure

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <main className="min-h-screen p-6">
+      <h1 className="text-3xl font-bold">Online Shop</h1>
+      <p className="mt-2 text-gray-600">Products will be displayed here.</p>
+    </main>
+  );
+}
